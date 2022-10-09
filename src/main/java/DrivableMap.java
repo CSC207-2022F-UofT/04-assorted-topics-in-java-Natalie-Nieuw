0fbb7defd8af7d02b5d43569cfe83e6e7072ae46
natalie-nieuw <natalie.nieuwenhuizen@mail.utoronto.ca>
2022-10-09 18:05:12 -0400

    Quiz 4

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -7,6 +7,7 @@
  * created the constructor for you already.
  */
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,8 +28,13 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
-
+    public boolean addDrivable(String identity, Drivable object) {
+        if (drivable_map.get(identity) == null) {
+            drivable_map.put(identity, object);
+            return true;
+        }
+        return false;
+    }
 
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
@@ -37,17 +43,26 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
-
-
-
+    public boolean hasFasterThan(int speed) {
+        for (Drivable item : drivable_map.values()) {
+            if (item.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     /* TODO: Write a method named getTradable that takes no arguments and
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
-
-
-
-    
+    public List<Tradable> getTradable() {
+        List<Tradable> result = new ArrayList<>();
+        for (Drivable item : drivable_map.values()) {
+            if (item instanceof Tradable) {
+                result.add((Tradable) item);
+            }
+        }
+        return result;
+    }
 }
